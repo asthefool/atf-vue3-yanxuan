e91d241c61d4902d1bf1a4e955095c01cd4b5696
欧阳康 <3161785980@qq.com>
2026-07-14 23:25:27 +0800

    fix: 补充在线演示的分类和新品静态数据

--- src/api/base.js
+++ src/api/base.js
@@ -1,12 +1,16 @@
 const isProduction = process.env.NODE_ENV === 'production'
 
 const base = {
-    baseUrl: 'https://you.163.com',
-    categoryUrl: '/api/xhr/globalinfo/queryTop.json',
-    findNew: '/api/xhr/item/getNewItem.json', //首页主体--新鲜好物
+    categoryUrl: isProduction
+        ? '/mock/category.json'
+        : '/api/xhr/globalinfo/queryTop.json',
+
+    findNew: isProduction
+        ? '/mock/new.json'
+        : '/api/xhr/item/getNewItem.json',
 
     bannerUrl: isProduction ? '/mock/banner.json' : '/foo/home/banner',
     findHot: isProduction ? '/mock/hot.json' : '/foo/home/hot',
-    product: isProduction ? '/mock/product.json' : '/foo/home/product' 
+    product: isProduction ? '/mock/product.json' : '/foo/home/product'
 }
 export default base;
