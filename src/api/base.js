9488be8c073d98a214cd9948a3694ffb89595671
欧阳康 <3161785980@qq.com>
2026-07-14 23:00:45 +0800

    feat: 为在线演示添加静态 Mock 数据

--- src/api/base.js
+++ src/api/base.js
@@ -1,9 +1,12 @@
-const base={
-    baseUrl:'https://you.163.com',
-    categoryUrl:'/api/xhr/globalinfo/queryTop.json',
-    bannerUrl:'/foo/home/banner',//轮播图
-    findNew:'/api/xhr/item/getNewItem.json', //首页主体--新鲜好物
-    findHot:'/foo/home/hot',
-    product:'/foo/home/product'
+const isProduction = process.env.NODE_ENV === 'production'
+
+const base = {
+    baseUrl: 'https://you.163.com',
+    categoryUrl: '/api/xhr/globalinfo/queryTop.json',
+    findNew: '/api/xhr/item/getNewItem.json', //首页主体--新鲜好物
+
+    bannerUrl: isProduction ? '/mock/banner.json' : '/foo/home/banner',
+    findHot: isProduction ? '/mock/hot.json' : '/foo/home/hot',
+    product: isProduction ? '/mock/product.json' : '/foo/home/product' 
 }
 export default base;
