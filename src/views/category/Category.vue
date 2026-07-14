cb3b3472a199893f65950bf39ac530f75bdaa0b8
欧阳康 <3161785980@qq.com>
2026-07-14 17:57:42 +0800

    feat: 完成分类页移动端响应式适配

--- src/views/category/Category.vue
+++ src/views/category/Category.vue
@@ -8,15 +8,15 @@
       </el-breadcrumb>
     </div>
     <!-- 2.轮播图 -->
-     <AppBanner :list='banner' />
+    <AppBanner :list="banner" />
 
     <!-- 3.分类展示 -->
      <div class="sub-list">
         <h3>全部分类</h3>
         <ul>
             <li v-for="item in category.subCateList" :key="item.id">
-                <a href="##">
-                    <img :src="item.bannerUrl" alt="">
+                <a href="#">
+                    <img :src="item.bannerUrl" :alt="item.name">
                     <div class="name">{{ item.name }}</div>
                 </a>
             </li>
@@ -68,37 +68,92 @@ export default {
 </script>
 
 <style lang="less" scoped>
-.category{
-    padding: 10px 40px;
-    .breadcrumb{
-        padding:20px 0;
+.category {
+  padding-top: 10px;
+  padding-bottom: 32px;
+
+  .breadcrumb {
+    padding: 20px 0;
+    overflow-x: auto;
+  }
+
+  .sub-list {
+    margin-top: 20px;
+
+    h3 {
+      font-size: 32px;
+      text-align: center;
+      font-weight: normal;
+      line-height: 100px;
     }
-    .sub-list{
-        margin-top: 20px;
-        h3{
-            font-size: 32px;
-            text-align: center;
-            font-weight: normal;
-            line-height: 100px;
+
+    ul {
+      display: grid;
+      grid-template-columns: repeat(6, minmax(0, 1fr));
+      gap: 16px;
+
+      li {
+        min-width: 0;
+        text-align: center;
+
+        a {
+          display: block;
+          padding: 12px 8px;
         }
-        ul{
-            display: flex;
-            flex-wrap: wrap;
-            justify-content: space-between;
-            li{
-                text-align: center;
-                width: 140px;
-                height: 160px;
-                img{
-                    width: 90px;
-                    height: 90px;
-                    margin-bottom:5px 0 ;
-                }
-                &:hover{
-                    .name{color: @xtxColor;}
-                }
-            }
+
+        img {
+          width: 90px;
+          height: 90px;
+          margin: 0 auto 8px;
+          object-fit: cover;
         }
+
+        &:hover .name {
+          color: @xtxColor;
+        }
+      }
     }
+  }
+}
+
+@media (max-width: 1023px) {
+  .category .sub-list ul {
+    grid-template-columns: repeat(4, minmax(0, 1fr));
+  }
+}
+
+@media (max-width: 767px) {
+  .category {
+    padding-top: 0;
+    padding-bottom: 24px;
+  }
+
+  .category .breadcrumb {
+    padding: 16px 0;
+  }
+
+  .category .sub-list {
+    margin-top: 8px;
+  }
+
+  .category .sub-list h3 {
+    font-size: 24px;
+    line-height: 72px;
+  }
+
+  .category .sub-list ul {
+    grid-template-columns: repeat(3, minmax(0, 1fr));
+    gap: 8px;
+  }
+
+  .category .sub-list ul li a {
+    padding: 8px 4px;
+  }
+
+  .category .sub-list ul li img {
+    width: min(80px, 100%);
+    height: auto;
+    aspect-ratio: 1;
+  }
 }
 </style>
